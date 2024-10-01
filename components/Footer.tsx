31d50e57f140178c00a1d28d8cf856c292142548
Ahmed Ali <ali0792@gmail.com>
2024-10-01 12:41:08 -0500

    Fix: type error in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,12 +39,7 @@ const Footer = () => {
           {socialMedia.map((profile) => (
             <a key={profile.id} href={profile.link} target="_blank">
               <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity75 bg-black-200 rounded-lg border border-black-300">
-                <img
-                  src={profile.img}
-                  alt={profile.id}
-                  width={20}
-                  height={20}
-                />
+                <img src={profile.img} width={20} height={20} />
               </div>
             </a>
           ))}
